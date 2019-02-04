c230e9a8c429f9ecaad83277042e517866a1b3cf
Martin Lindahl Ubuntu <ml224mn@student.lnu.se>
2019-02-04 17:05:10 +0100

    Made a testing route to send messages

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const json = require("koa-json");
 const KoaRouter = require("koa-router");
 const BodyParser = require('koa-bodyparser')
+const request = require('request')
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -20,13 +21,34 @@ app.use(BodyParser())
 app.use(router.routes()).use(router.allowedMethods());
 
 // test route
-router.get("/", ctx => (ctx.body = message));
+router.get("/", ctx => {
+  let request_body = {
+    "recipient": {
+      "id": "MartinoLindahl"
+    },
+    "message": "Tjenamors!"
+  }
+
+  request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+  ctx.redirect('/')
+});
 
 // Verify route
-router.post('/webhook', ctx => {
-  message = ctx.body
+// router.post('/webhook', ctx => {
+//   message = ctx.body
   
-})
+// })
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server started.");
